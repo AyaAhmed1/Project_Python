fc26610dca3d44e5821555f6dffd1bbecbca2005
ahmedkamel1192 <ahmed.kamel1192@gmail.com>
2018-02-13 19:46:52 +0200

    remove class User from models and import class user

--- socialmedia/socialapp/models.py
+++ socialmedia/socialapp/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     category_name = models.CharField(max_length = 50)
+    cat_img = models.CharField(max_length = 255)
 
 
 class Posts(models.Model):
