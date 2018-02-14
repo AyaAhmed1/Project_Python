5f3caf000820639744ea06d72329b45595f68446
ghadakhamis <ghada.khamis.m@gmail.com>
2018-02-14 17:42:33 +0200

    add validation

--- socialmedia/socialapp/forms.py
+++ socialmedia/socialapp/forms.py
@@ -1,9 +1,52 @@
+import re
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ObjectDoesNotExist
 
-class RegistrationForm(UserCreationForm):
-	email=forms.EmailField(required=True)
-	class Meta:
-		model=User
-		fields=('username','email')
+
+#class RegistrationForm(UserCreationForm):
+	#email=forms.EmailField(required=True)
+	#class Meta:
+		#model=User
+		#fields=('username','email')
+
+class RegistrationForm(forms.Form):
+
+	username=forms.CharField(label='Username',max_length=30)
+	email=forms.EmailField(label='Email')
+	password=forms.CharField(label='Password',widget=forms.PasswordInput())
+	passwordc=forms.CharField(label='Password confirmation',widget=forms.PasswordInput())
+	
+	def clean_passwordc(self):
+		if 'password' in self.cleaned_data:
+			password=self.cleaned_data['password']
+			passwordc=self.cleaned_data['passwordc']
+			if password == passwordc:
+				return password
+		raise forms.ValidationError('Passwords do not match.')
+
+	def clean_username(self):
+		username=self.cleaned_data['username']
+		if not re.search(r'^\w+$',username):
+			raise forms.ValidationError('username can only contain alphanumeric characters and the underscore.')
+		try:
+			User.objects.get(username=username)
+		except ObjectDoesNotExist:
+			return username
+		raise forms.ValidationError('username is already taken.')		
+
+	def clean_email(self):
+		email=self.cleaned_data['email']
+		try:
+			User.objects.get(email=email)
+		except ObjectDoesNotExist:
+			return email
+		raise forms.ValidationError('email is already taken.')
+
+
+
+
+
+
+
--- socialmedia/socialapp/views.py
+++ socialmedia/socialapp/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .forms import RegistrationForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import *
 
 # Create your views here.
 
@@ -11,7 +14,8 @@ def signUp(request):
 	if request.method=="POST":
 		user_form=RegistrationForm(request.POST)
 		if user_form.is_valid():
-			user_form.save()
+			user=User.objects.create(username=user_form.cleaned_data['username'],password=user_form.cleaned_data['password'],email=user_form.cleaned_data['email'])
+			#user.save()
 			return HttpResponseRedirect("/socialapp/signup/")
 	context={"form":user_form}
 	return render(request,"user/new.html",context)
