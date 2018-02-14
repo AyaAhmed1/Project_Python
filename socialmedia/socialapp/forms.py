3e8a7d00cf13a044232ff2713350a437bdce93e5
ghadakhamis <ghada.khamis.m@gmail.com>
2018-02-14 18:11:56 +0200

    change validation

--- socialmedia/socialapp/forms.py
+++ socialmedia/socialapp/forms.py
@@ -4,27 +4,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ObjectDoesNotExist
 
-
-#class RegistrationForm(UserCreationForm):
-	#email=forms.EmailField(required=True)
-	#class Meta:
-		#model=User
-		#fields=('username','email')
-
-class RegistrationForm(forms.Form):
+class RegistrationForm(UserCreationForm):
 
 	username=forms.CharField(label='Username',max_length=30)
 	email=forms.EmailField(label='Email')
-	password=forms.CharField(label='Password',widget=forms.PasswordInput())
-	passwordc=forms.CharField(label='Password confirmation',widget=forms.PasswordInput())
-	
-	def clean_passwordc(self):
-		if 'password' in self.cleaned_data:
-			password=self.cleaned_data['password']
-			passwordc=self.cleaned_data['passwordc']
-			if password == passwordc:
-				return password
-		raise forms.ValidationError('Passwords do not match.')
+	class Meta:
+		model=User
+		fields=('username','email',)
 
 	def clean_username(self):
 		username=self.cleaned_data['username']
--- socialmedia/socialapp/views.py
+++ socialmedia/socialapp/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from .forms import RegistrationForm
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
-from django.contrib.auth.forms import *
 
 # Create your views here.
 
@@ -14,8 +11,7 @@ def signUp(request):
 	if request.method=="POST":
 		user_form=RegistrationForm(request.POST)
 		if user_form.is_valid():
-			user=User.objects.create(username=user_form.cleaned_data['username'],password=user_form.cleaned_data['password'],email=user_form.cleaned_data['email'])
-			#user.save()
+			user_form.save()
 			return HttpResponseRedirect("/socialapp/signup/")
 	context={"form":user_form}
 	return render(request,"user/new.html",context)
