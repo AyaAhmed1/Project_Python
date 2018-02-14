ff37fc8359b7e82b36f86eaf3d36a540d6115cc6
ghadakhamis <ghada.khamis.m@gmail.com>
2018-02-14 14:23:50 +0200

    registration form

--- socialmedia/socialapp/views.py
+++ socialmedia/socialapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import RegistrationForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def signUp(request):
+	user_form=RegistrationForm()
+	if request.method=="POST":
+		user_form=RegistrationForm(request.POST)
+		if user_form.is_valid():
+			user_form.save()
+			return HttpResponseRedirect("/socialapp/signup/")
+	context={"form":user_form}
+	return render(request,"user/new.html",context)
